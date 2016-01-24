3c9a11589559f65fb92e0bdf4653fff5b48a3802
vmyilsamy <veeraraghavanm@gmail.com>
2016-01-24 19:41:15 +0000

    Create run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,40 @@
+library(data.table)
+
+#Read from the analysis data
+testData <- read.table("./UCI HAR Dataset/test/X_test.txt",header=FALSE)
+testData_act <- read.table("./UCI HAR Dataset/test/y_test.txt",header=FALSE)
+testData_sub <- read.table("./UCI HAR Dataset/test/subject_test.txt",header=FALSE)
+trainData <- read.table("./UCI HAR Dataset/train/X_train.txt",header=FALSE)
+trainData_act <- read.table("./UCI HAR Dataset/train/y_train.txt",header=FALSE)
+trainData_sub <- read.table("./UCI HAR Dataset/train/subject_train.txt",header=FALSE)
+
+#Uses descriptive activity names to name the activities
+activities <- read.table("./UCI HAR Dataset/activity_labels.txt",header=FALSE,colClasses="character")
+testData_act$V1 <- factor(testData_act$V1,levels=activities$V1,labels=activities$V2)
+trainData_act$V1 <- factor(trainData_act$V1,levels=activities$V1,labels=activities$V2)
+
+#Appropriately labels the data set with descriptive activity names
+features <- read.table("./UCI HAR Dataset/features.txt",header=FALSE,colClasses="character")
+colnames(testData)<-features$V2
+colnames(trainData)<-features$V2
+colnames(testData_act)<-c("Activity")
+colnames(trainData_act)<-c("Activity")
+colnames(testData_sub)<-c("Subject")
+colnames(trainData_sub)<-c("Subject")
+
+#Merge test and training sets into one data set
+testData<-cbind(testData,testData_act)
+testData<-cbind(testData,testData_sub)
+trainData<-cbind(trainData,trainData_act)
+trainData<-cbind(trainData,trainData_sub)
+bigData<-rbind(testData,trainData)
+
+#Extract only the measurements on the mean and standard deviation for each measurement
+bigData_mean<-sapply(bigData,mean,na.rm=TRUE)
+bigData_sd<-sapply(bigData,sd,na.rm=TRUE)
+
+#Creates a second, independent tidy data set with the average of 
+#each variable for each activity and each subject.
+dtAvg <- data.table(bigData)
+tidy<-dtAvg[,lapply(.SD,mean),by="Activity,Subject"]
+write.table(tidy,file="tidy.csv",sep=",",row.names = FALSE)
